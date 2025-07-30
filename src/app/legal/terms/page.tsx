855045f6f8af0376efa2e2b476da76030c5a534e
Kevin D'Amato <kevindamato0809@gmail.com>
2025-07-30 12:57:07 +0200

    Create page.tsx

---
+++ src/app/legal/terms/page.tsx
@@ -0,0 +1,31 @@
+const TermsOfService = () => {
+  return (
+    <div className="container p-4">
+      <h1>Termini di Servizio</h1>
+      <p>Ultimo aggiornamento: 30 Luglio 2025</p>
+      <p>Benvenuto su TastieriniKD! Questi termini di servizio delineano le regole e i regolamenti per l&apos;uso del sito web di TastieriniKD, situato all&apos;indirizzo https://tastierinikd.vercel.app.</p>
+      <p>Accedendo a questo sito web, assumiamo che tu accetti questi termini di servizio. Non continuare a usare TastieriniKD se non accetti tutti i termini e le condizioni indicati in questa pagina.</p>
+
+      <h2>Licenza</h2>
+      <p>Salvo diversa indicazione, TastieriniKD e/o i suoi licenziatari possiedono i diritti di proprietà intellettuale per tutto il materiale su TastieriniKD. Tutti i diritti di proprietà intellettuale sono riservati. Puoi accedere a questo da TastieriniKD per il tuo uso personale soggetto alle restrizioni stabilite in questi termini di servizio.</p>
+
+      <p>Non devi:</p>
+      <ul>
+        <li>Ripubblicare materiale da TastieriniKD</li>
+        <li>Vendere, noleggiare o sub-licenziare materiale da TastieriniKD</li>
+        <li>Riprodurre, duplicare o copiare materiale da TastieriniKD</li>
+        <li>Ridistribuire contenuti da TastieriniKD</li>
+      </ul>
+
+      <h2>Riserva dei Diritti</h2>
+      <p>Ci riserviamo il diritto di richiedere la rimozione di tutti i link o di qualsiasi link particolare al nostro sito web. Accetti di rimuovere immediatamente tutti i link al nostro sito web su richiesta. Ci riserviamo inoltre il diritto di modificare questi termini di servizio e la sua politica di collegamento in qualsiasi momento. Accedendo continuamente al nostro sito web, accetti di essere vincolato e di seguire questi termini di servizio.</p>
+
+      <h2>Rimozione di link dal nostro sito web</h2>
+      <p>Se trovi qualsiasi link sul nostro sito web che sia offensivo per qualsiasi motivo, sei libero di contattarci e informarci in qualsiasi momento. Prenderemo in considerazione le richieste di rimozione dei link, ma non siamo obbligati a farlo o a risponderti direttamente.</p>
+
+      <p>Non garantiamo che le informazioni su questo sito web siano corrette, non garantiamo la loro completezza o accuratezza; né promettiamo di garantire che il sito web rimanga disponibile o che il materiale sul sito web sia mantenuto aggiornato.</p>
+    </div>
+  );
+};
+
+export default TermsOfService;
