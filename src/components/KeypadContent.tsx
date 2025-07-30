7431bcfefe1ad28d6c9ecdde3975a8dd163958e2
Kevin D'Amato <kevindamato0809@gmail.com>
2025-07-30 13:21:06 +0200

    Create KeypadContent.tsx

---
+++ src/components/KeypadContent.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import KeypadPreview from './KeypadPreview';
+
+const KeypadContent = () => {
+  const searchParams = useSearchParams();
+
+  const keypadType = searchParams.get('keypadType') || 'Numerico';
+  const question = searchParams.get('question') || '';
+  const secretCode = searchParams.get('secretCode') || '';
+  const caseSensitive = searchParams.get('caseSensitive') === 'true';
+  const font = searchParams.get('font') || 'Arial';
+  const successMessage = searchParams.get('successMessage') || 'Corretto!';
+  const successColor = searchParams.get('successColor') || '#00ff00';
+  const errorMessage = searchParams.get('errorMessage') || 'Errato, riprova.';
+  const errorColor = searchParams.get('errorColor') || '#ff0000';
+  const transparentBg = searchParams.get('transparentBg') === 'true';
+
+  return (
+    <KeypadPreview 
+      keypadType={keypadType} 
+      question={question} 
+      font={font} 
+      secretCode={secretCode}
+      caseSensitive={caseSensitive}
+      successMessage={successMessage}
+      successColor={successColor}
+      errorMessage={errorMessage}
+      errorColor={errorColor}
+      transparentBg={transparentBg}
+      isPreview={false}
+    />
+  );
+};
+
+export default KeypadContent;
