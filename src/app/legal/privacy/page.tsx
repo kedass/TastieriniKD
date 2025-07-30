b30775c04e7eed023973094e3b298078dc12ff3c
Kevin D'Amato <kevindamato0809@gmail.com>
2025-07-30 12:57:03 +0200

    Create page.tsx

---
+++ src/app/legal/privacy/page.tsx
@@ -0,0 +1,17 @@
+const PrivacyPolicy = () => {
+  return (
+    <div className="container p-4">
+      <h1>Privacy Policy</h1>
+      <p>Ultimo aggiornamento: 30 Luglio 2025</p>
+      <p>La tua privacy è importante per noi. È politica di TastieriniKD rispettare la tua privacy riguardo a qualsiasi informazione che possiamo raccogliere da te attraverso il nostro sito web, https://tastierinikd.vercel.app, e altri siti che possediamo e gestiamo.</p>
+      <p>Richiediamo informazioni personali solo quando ne abbiamo veramente bisogno per fornirti un servizio. Le raccogliamo con mezzi equi e legali, con la tua conoscenza e il tuo consenso. Ti informiamo anche del perché le raccogliamo e di come verranno utilizzate.</p>
+      <p>Conserviamo le informazioni raccolte solo per il tempo necessario a fornirti il servizio richiesto. I dati che conserviamo, li proteggeremo con mezzi commercialmente accettabili per prevenire perdite e furti, nonché accessi, divulgazione, copia, uso o modifica non autorizzati.</p>
+      <p>Non condividiamo alcuna informazione di identificazione personale pubblicamente o con terze parti, tranne quando richiesto dalla legge.</p>
+      <p>Il nostro sito web può contenere link a siti esterni che non sono gestiti da noi. Si prega di essere consapevoli del fatto che non abbiamo alcun controllo sul contenuto e sulle pratiche di questi siti e non possiamo assumerci la responsabilità per le loro rispettive politiche sulla privacy.</p>
+      <p>Sei libero di rifiutare la nostra richiesta di informazioni personali, con la consapevolezza che potremmo non essere in grado di fornirti alcuni dei servizi desiderati.</p>
+      <p>L&apos;uso continuato del nostro sito web sarà considerato come accettazione delle nostre pratiche in materia di privacy e informazioni personali. Se hai domande su come gestiamo i dati degli utenti e le informazioni personali, non esitare a contattarci.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
