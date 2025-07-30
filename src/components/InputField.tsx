b5cb5de4d770a287d09667d1180227537790c466
Kevin D'Amato <kevindamato0809@gmail.com>
2025-07-30 12:57:09 +0200

    Create InputField.tsx

---
+++ src/components/InputField.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Button, InputGroup, FormControl } from 'react-bootstrap';
+
+interface InputFieldProps {
+  secretCode: string;
+  caseSensitive: boolean;
+  onSuccess: () => void;
+  onError: () => void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ secretCode, caseSensitive, onSuccess, onError }) => {
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmitClick = () => {
+    const finalInput = caseSensitive ? input : input.toLowerCase();
+    const finalSecretCode = caseSensitive ? secretCode : secretCode.toLowerCase();
+
+    if (finalInput === finalSecretCode) {
+      onSuccess();
+    } else {
+      onError();
+    }
+    setInput('');
+  };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <InputGroup style={{ width: '300px' }}>
+        <FormControl placeholder="Inserisci la risposta..." value={input} onChange={handleInputChange} />
+        <Button variant="success" onClick={handleSubmitClick}>Invia</Button>
+      </InputGroup>
+    </div>
+  );
+};
+
+export default InputField;
