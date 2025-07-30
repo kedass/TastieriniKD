2ea603a41f88d9de71449a27e05dee4c8a0efb12
Kevin D'Amato <kevindamato0809@gmail.com>
2025-07-30 12:57:13 +0200

    Create page.tsx

---
+++ src/app/legal/cookies/page.tsx
@@ -0,0 +1,36 @@
+const CookiePolicy = () => {
+  return (
+    <div className="container p-4">
+      <h1>Cookie Policy</h1>
+      <p>Ultimo aggiornamento: 30 Luglio 2025</p>
+      <p>Questa è la Cookie Policy per TastieriniKD, accessibile da https://tastierinikd.vercel.app</p>
+
+      <h2>Cosa sono i cookie</h2>
+      <p>Come è prassi comune con quasi tutti i siti web professionali, questo sito utilizza i cookie, che sono piccoli file scaricati sul tuo computer, per migliorare la tua esperienza. Questa pagina descrive quali informazioni raccolgono, come le usiamo e perché a volte abbiamo bisogno di memorizzare questi cookie. Ti diremo anche come puoi impedire che questi cookie vengano memorizzati, tuttavia ciò potrebbe declassare o &apos;rompere&apos; alcuni elementi della funzionalità del sito.</p>
+
+      <h2>Come utilizziamo i cookie</h2>
+      <p>Utilizziamo i cookie per una serie di motivi dettagliati di seguito. Sfortunatamente, nella maggior parte dei casi non ci sono opzioni standard del settore per disabilitare i cookie senza disabilitare completamente la funzionalità e le caratteristiche che aggiungono a questo sito. Si consiglia di lasciare attivi tutti i cookie se non si è sicuri di averne bisogno o meno nel caso in cui vengano utilizzati per fornire un servizio che si utilizza.</p>
+
+      <h2>Disabilitazione dei cookie</h2>
+      <p>È possibile impedire l&apos;impostazione dei cookie regolando le impostazioni sul browser (vedere la Guida del browser per come fare). Si prega di essere consapevoli del fatto che la disabilitazione dei cookie influenzerà la funzionalità di questo e di molti altri siti web che visiti. La disabilitazione dei cookie di solito comporta anche la disabilitazione di alcune funzionalità e caratteristiche di questo sito. Pertanto si consiglia di non disabilitare i cookie.</p>
+
+      <h2>I cookie che impostiamo</h2>
+      <ul>
+        <li>Cookie relativi alle preferenze del sito: Al fine di fornirti un&apos;ottima esperienza su questo sito, forniamo la funzionalità per impostare le tue preferenze su come questo sito funziona quando lo usi. Per ricordare le tue preferenze, dobbiamo impostare i cookie in modo che queste informazioni possano essere richiamate ogni volta che interagisci con una pagina influenzata dalle tue preferenze.</li>
+      </ul>
+
+      <h2>Cookie di terze parti</h2>
+      <p>In alcuni casi speciali utilizziamo anche cookie forniti da terze parti fidate. La seguente sezione descrive in dettaglio quali cookie di terze parti potresti incontrare tramite questo sito.</p>
+      <ul>
+        <li>Questo sito utilizza Google Analytics, che è una delle soluzioni di analisi più diffuse e affidabili sul web per aiutarci a capire come utilizzi il sito e come possiamo migliorare la tua esperienza. Questi cookie possono tracciare cose come il tempo che trascorri sul sito e le pagine che visiti in modo da poter continuare a produrre contenuti coinvolgenti.</li>
+        <li>Per maggiori informazioni sui cookie di Google Analytics, consulta la pagina ufficiale di Google Analytics.</li>
+      </ul>
+
+      <h2>Maggiori informazioni</h2>
+      <p>Speriamo che questo abbia chiarito le cose per te e, come accennato in precedenza, se c&apos;è qualcosa di cui non sei sicuro se hai bisogno o meno, di solito è più sicuro lasciare i cookie abilitati nel caso in cui interagiscano con una delle funzionalità che utilizzi sul nostro sito.</p>
+      <p>Tuttavia, se stai ancora cercando maggiori informazioni, puoi contattarci tramite uno dei nostri metodi di contatto preferiti.</p>
+    </div>
+  );
+};
+
+export default CookiePolicy;
