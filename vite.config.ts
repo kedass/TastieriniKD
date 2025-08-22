ea4069b6b6750c0dd52942e94d537c47890dbe30
Kevin D'Amato <kevindamato0809@gmail.com>
2025-08-22 19:44:54 +0200

    e

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TastieriniKD/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-  },
 })
